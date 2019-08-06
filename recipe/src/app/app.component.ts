dde54d0bf6be25dcfc4f4c077db1a35022a5bfd9
wsyang0309 <wsyang0309@gmail.com>
2019-08-06 02:38:17 -0400

    v1

--- recipe/src/app/app.component.ts
+++ recipe/src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe';
+  isHamburger = false;
+  breadCounter = 0;
+  beefCounter = 0;
+
+  addBread(){
+    this.breadCounter ++;
+    this.checkRecipe();
+  }
+
+  addBeef(){
+    this.beefCounter ++;
+    this.checkRecipe();
+  }
+
+  checkRecipe(){
+    if( this.breadCounter >= 2 && this.beefCounter >= 1){
+      this.isHamburger = true;
+    }
+  }
+
 }
---
+++ recipe/src/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient{
+    constructor(public name: string,
+                public count: 0) {
+    }
+  }
